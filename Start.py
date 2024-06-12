221eb4eee388d2becfdb3b9b6d49d02264b0435a
Perry Wong <125176864+lizzary@users.noreply.github.com>
2024-06-12 13:04:15 +0800

    Update Start.py

--- Start.py
+++ Start.py
@@ -1,4 +1,10 @@
 from package.ui import runUI
 
 if __name__ == '__main__':
+    try:
+        import pyi_splash
+        pyi_splash.close()
+    except ImportError:
+        pass
+
     runUI.run()
